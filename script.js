980be65bcd66f815522b3f1ad22262584d50a943
Hayden <hbak0001@student.monash.edu>
2018-09-20 23:47:15 +1000

    Put everything in right place. Made Brush work right etc.

--- script.js
+++ script.js
@@ -3,16 +3,21 @@ var canvas = document.querySelector( '#canvas' );
 var context = canvas.getContext( '2d' );
 var linePoints = [];
 var toolMode = 'draw'
-var toolSize = 5;
+var toolSize = 10;
+var toolSizePerm = 10;
+var lastPoint = null;
 var toolColor = '#000000'
 var canvasState = [];
 var undoButton = document.querySelector( '[data-action=undo]' );
+var autoErase;
+var eraseTimerStart = false;
+var autoEraseAlert = null;
 window.addEventListener( 'resize', resizeCanvas );
 resizeCanvas();
 
 // Defaults
 context.strokeStyle = "#000000";
-context.lineWidth = 5;
+context.lineWidth = 10;
 context.lineJoin = "round";
 context.lineCap = "round";
 
@@ -25,39 +30,101 @@ document.querySelector( '#tools' ).addEventListener( 'click', selectTool );
 document.querySelector( '#colors' ).addEventListener( 'click', selectTool );
 
 // Functions
+function cancel() {
+	clearTimeout(autoErase);
+	document.getElementById("alert-message").style.zIndex = -1;
+	document.getElementById("canvas").style.pointerEvents = "auto";
+	eraseTimerStart = false;
+	document.getElementById("message-text").innerText = "If you're still drawing, let us know by touching below. The drawing will erase in 15 seconds.";
+	if(document.getElementById("canvas").style.pointerEvents !== "none" & eraseTimerStart === false){
+		eraseTimerStart = true;
+		autoEraseAlert = setTimeout(clearAlert, 10000);
+	}
+}
+
+function clearAlert(){
+	document.getElementById("alert-message").style.zIndex = 2;
+	document.getElementById("canvas").style.pointerEvents = "none";
+	autoErase = setTimeout(clearCanvas, 16000);
+	reduce(15);
+}
+
 function clearCanvas() {
-  var result = confirm( 'Are you sure you want to delete the picture?' );
-  if ( result ) {
     context.clearRect( 0, 0, canvas.width, canvas.height );
     canvasState.length = 0;
     undoButton.classList.add( 'disabled' );
-  }
+	document.getElementById("alert-message").style.zIndex = -1;
+	document.getElementById("canvas").style.pointerEvents = "auto";
+	eraseTimerStart = false;
 }
 
 function draw( e ) {
+	if (autoEraseAlert != null) {
+		clearTimeout(autoEraseAlert);
+		eraseTimerStart = false;
+	}
   if ( e.which === 1 || e.type === 'touchstart' || e.type === 'touchmove') {
     window.addEventListener( 'mousemove', draw );
     window.addEventListener( 'touchmove', draw );
     var mouseX = e.pageX - canvas.offsetLeft;
     var mouseY = e.pageY - canvas.offsetTop;
     var mouseDrag = e.type === 'mousemove';
-      var swag = Math.random();
-      if (swag < 0.33){
-            toolSize = toolSize + 1; 
-        }
-        else if (swag < 0.66  && toolSize > 5){
-            toolSize = toolSize - 1; 
-        }
+	var swag = Math.random()*10+10;
+	var kush = Math.random()*10+10;
     if ( e.type === 'touchstart' || e.type === 'touchmove' ) {
         //console.log( e );
         mouseX = e.touches[0].pageX - canvas.offsetLeft;
         mouseY = e.touches[0].pageY - canvas.offsetTop;
         mouseDrag = e.type === 'touchmove';
     }
-    if ( e.type === 'mousedown' || e.type === 'touchstart') saveState();        
-    linePoints.push( { x: mouseX, y: mouseY, drag: mouseDrag, width: toolSize, color: toolColor } );
+	/*	Splash stuff
+	var banter = Math.random() - 0.5;
+	if (banter > 0){
+		banter = 1;
+	}
+	else{
+		banter = -1;
+	}
+	var memes = Math.random() - 0.5;
+	if (memes > 0){
+		memes = 1;
+	}
+	else{
+		memes = -1;
+	}
+	var splash = null;
+	if ( e.type === 'mousedown'){
+		splash = {x: mouseX + banter*swag, y: mouseY + memes*kush};
+	}
+		console.log(e);*/
+	if (lastPoint !== null){
+		var distX = mouseX - lastPoint.x;
+		var distY = mouseY - lastPoint.y;
+		var dist = Math.sqrt(distX*distX + distY*distY);
+	} 
+	else{
+		var dist = 0;
+	}
+	if (dist < 50){
+		toolSize = toolSizePerm - (toolSizePerm-1)*dist/50;
+	}
+	else{
+		toolSize = 1;
+	}
+    if ( e.type === 'mousedown' || e.type === 'touchstart') saveState();  
+	//lastPoint = { x: mouseX, y: mouseY, drag: mouseDrag, width: toolSize, color: toolColor, splash: splash};
+	lastPoint = { x: mouseX, y: mouseY, drag: mouseDrag, width: toolSize, color: toolColor};
+    linePoints.push( lastPoint );
     updateCanvas();
   }
+}
+
+function reduce(sec){
+	var alertMessage = document.getElementById("message-text");
+	if (sec >= 0 && eraseTimerStart == true) {
+		alertMessage.innerText = "If you're still drawing, let us know by touching below. The drawing will erase in "+sec+" seconds.";
+		var repeat = setTimeout(reduce, 1000, sec-1);
+	}
 }
 
 function highlightButton( button ) {
@@ -69,18 +136,28 @@ function highlightButton( button ) {
 function renderLine() {
   for ( var i = 0, length = linePoints.length; i < length; i++ ) {
       context.lineWidth = linePoints[i].width; 
+
     if ( !linePoints[i].drag ) {
       //context.stroke();
       context.beginPath();
-        
+
     //context.lineWidth = linePoints[i].width; 
       context.strokeStyle = linePoints[i].color;
       context.moveTo( linePoints[i].x, linePoints[i].y );
       context.lineTo( linePoints[i].x + 0.5, linePoints[i].y + 0.5 );
-    } else {
+    }
+	else {
         context.moveTo( linePoints[i-1].x, linePoints[i-1].y );
       context.lineTo( linePoints[i].x, linePoints[i].y );
     }
+      context.stroke();
+      context.beginPath();
+	/*	Splash dot stuff
+	if (linePoints[i].splash != null){
+		context.arc(linePoints[i].splash.x, linePoints[i].splash.y, 2, 2*Math.PI, false);
+		context.fillStyle = linePoints[i].color;
+		context.fill();
+	}*/
       context.stroke();
       context.beginPath();
   }
@@ -108,9 +185,14 @@ function saveState() {
 }
 
 function selectTool( e ) {
+	if (autoEraseAlert != null) {
+		clearTimeout(autoEraseAlert);
+		eraseTimerStart = false;
+	}
   if ( e.target === e.currentTarget ) return;
   if ( !e.target.dataset.action ) highlightButton( e.target );
   toolSize = e.target.dataset.size || toolSize;
+  toolSizePerm = e.target.dataset.size || toolSize;
   toolMode = e.target.dataset.mode || toolMode;
   toolColor = e.target.dataset.color || toolColor;
   if ( e.target === undoButton ) undoState();
@@ -121,6 +203,10 @@ function stop( e ) {
   if ( e.which === 1 || e.type === 'touchend') {
     window.removeEventListener( 'mousemove', draw );
     window.removeEventListener( 'touchmove', draw );
+  }
+  if(document.getElementById("canvas").style.pointerEvents !== "none" & eraseTimerStart === false){
+	  eraseTimerStart = true;
+	autoEraseAlert = setTimeout(clearAlert, 10000);
   }
 }
 
